314369747696b045510532a6b24a347b9f9aa90f
智昭 金山 <mushimushikera@live.jp>
2020-01-08 15:53:08 +0900

    validate date

--- mclient/src/reducer/timelapseSettings.ts
+++ mclient/src/reducer/timelapseSettings.ts
@@ -30,58 +30,54 @@ const initialState: TimeLapseState = {
 
 export default (state = initialState, action: Action): TimeLapseState => {
   if (isType(action, actions.setBounded)) {
+    const {
+      rangeStartDate,
+      rangeEndDate,
+      selectedStartDate,
+      selectedEndDate
+    } = state;
     const { payload } = action;
+    const { start, end, lowerCorner, upperCorner } = payload;
     return {
       ...state,
-      startDate: payload.start,
-      endDate: payload.end,
-      lowerCorner: payload.lowerCorner,
-      upperCorner: payload.upperCorner
-    };
-  }
-  if (isType(action, actions.setSelectedStartDate)) {
-    const { payload } = action;
-    const { selectedStartDate, selectedEndDate } = state;
-    const delta = (() => {
-      if (!selectedStartDate || !selectedEndDate) {
-        return defaultDuration;
-      }
-      return selectedEndDate.getTime() - selectedStartDate.getTime();
-    })();
-    const endDate = new Date(payload.getTime() + delta);
-    console.log('selected date');
-    console.log(delta);
-    console.log(payload);
-    console.log(endDate);
-    return {
-      ...state,
-      selectedStartDate: payload,
-      selectedEndDate: endDate
-    };
-  }
-  if (isType(action, actions.setSelectedEndDate)) {
-    const { payload } = action;
-    return {
-      ...state,
-      selectedEndDate: payload
+      startDate: start,
+      endDate: end,
+      rangeStartDate:
+        rangeStartDate.getTime() < start.getTime() ? start : rangeStartDate,
+      rangeEndDate: rangeEndDate.getTime() > end.getTime() ? end : rangeEndDate,
+      selectedStartDate:
+        selectedStartDate.getTime() < start.getTime()
+          ? start
+          : selectedStartDate,
+      selectedEndDate:
+        selectedEndDate.getTime() > end.getTime() ? end : selectedEndDate,
+      lowerCorner,
+      upperCorner
     };
   }
   if (isType(action, actions.setRangeStartDate)) {
     const { payload } = action;
-    const { rangeStartDate, rangeEndDate } = state;
+    const { rangeStartDate, rangeEndDate, endDate } = state;
     const delta = (() => {
       if (!rangeStartDate || !rangeEndDate) {
         return defaultRange;
       }
       return rangeEndDate.getTime() - rangeStartDate.getTime();
     })();
-    debugger;
+    let newRangeEndDate = new Date(payload.getTime() + delta);
+    if (newRangeEndDate.getTime() > endDate.getTime()) {
+      newRangeEndDate = endDate;
+    }
+    let newSelectedEndDate = new Date(payload.getTime() + defaultDuration);
+    if (newSelectedEndDate.getTime() > newRangeEndDate.getTime()) {
+      newSelectedEndDate = newRangeEndDate;
+    }
     return {
       ...state,
       rangeStartDate: payload,
-      rangeEndDate: new Date(payload.getTime() + delta),
+      rangeEndDate: newRangeEndDate,
       selectedStartDate: payload,
-      selectedEndDate: new Date(payload.getTime() + defaultDuration)
+      selectedEndDate: newSelectedEndDate
     };
   }
   if (isType(action, actions.setRangeEndDate)) {
@@ -91,6 +87,35 @@ export default (state = initialState, action: Action): TimeLapseState => {
       return {
         ...state,
         rangeEndDate: payload
+      };
+    }
+  }
+  if (isType(action, actions.setSelectedStartDate)) {
+    const { payload } = action;
+    const { selectedStartDate, selectedEndDate, rangeEndDate } = state;
+    const delta = (() => {
+      if (!selectedStartDate || !selectedEndDate) {
+        return defaultDuration;
+      }
+      return selectedEndDate.getTime() - selectedStartDate.getTime();
+    })();
+    let endDate = new Date(payload.getTime() + delta);
+    if (endDate.getTime() > rangeEndDate.getTime()) {
+      endDate = rangeEndDate;
+    }
+    return {
+      ...state,
+      selectedStartDate: payload,
+      selectedEndDate: endDate
+    };
+  }
+  if (isType(action, actions.setSelectedEndDate)) {
+    const { selectedStartDate } = state;
+    const { payload } = action;
+    if (payload.getTime() > selectedStartDate.getTime()) {
+      return {
+        ...state,
+        selectedEndDate: payload
       };
     }
   }
